0ed1c1ce61e755233629b714201dc3840bb0cc33
Rafael Anastácio Alves <rafaelanastacioalves@gmail.com>
2023-08-01 00:08:24 -0300

    refactor: brings data generation list in repository for better separation of concerns

--- app/src/main/java/com/example/rafaelanastacioalves/moby/domain/interactors/MainEntityListInteractor.kt
+++ app/src/main/java/com/example/rafaelanastacioalves/moby/domain/interactors/MainEntityListInteractor.kt
@@ -7,24 +7,15 @@ import com.example.rafaelanastacioalves.moby.repository.AppRepository
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 
-class MainEntityListInteractor(appRepository: AppRepository) :
+class MainEntityListInteractor(val appRepository: AppRepository) :
         Interactor<Resource<List<MainEntity>>, MainEntityListInteractor.RequestValues>() {
 
     override suspend fun run(requestValues: RequestValues?, flowCollector: FlowCollector<Resource<List<MainEntity>>>){
-        var finalList: MutableList<MainEntity> = ArrayList<MainEntity>()
-        var number = 0
-        while (true) {
-            delay(2000)
-            number++
-            Log.d(javaClass.name, "emitting in flow a number value $number")
-            finalList.add(0,
-                MainEntity("$number","Entity number $number", "price", "reais", "url")
-            )
-            flowCollector.emit(
-                Resource.success(finalList)
-            )
 
-        }
+
+        flowCollector.emitAll(
+            appRepository.mainEntity()
+        )
 
 
     }
--- app/src/main/java/com/example/rafaelanastacioalves/moby/repository/AppRepository.kt
+++ app/src/main/java/com/example/rafaelanastacioalves/moby/repository/AppRepository.kt
@@ -1,36 +1,40 @@
 package com.example.rafaelanastacioalves.moby.repository
 
+import android.util.Log
 import com.example.rafaelanastacioalves.moby.domain.model.EntityDetails
 import com.example.rafaelanastacioalves.moby.domain.model.MainEntity
 import com.example.rafaelanastacioalves.moby.domain.model.Resource
 import com.example.rafaelanastacioalves.moby.repository.database.DAO
 import com.example.rafaelanastacioalves.moby.repository.http.APIClient
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
 
 class AppRepository(
     private val appDao: DAO,
     var apiClient: APIClient) {
 
-    suspend fun mainEntity(): Resource<List<MainEntity>> {
-        return object : NetworkBoundResource<List<MainEntity>, List<MainEntity>>() {
-            override suspend fun fecthFromHttp(): List<MainEntity> {
+    suspend fun mainEntity(): Flow<Resource<List<MainEntity>>> {
 
-                return apiClient.getMainEntityList()
+        return flow {
+            val finalList: MutableList<MainEntity> = ArrayList<MainEntity>()
+            var number = 0
+            while (true) {
+                delay(2000)
+                number++
+                Log.d(javaClass.name, "emitting in flow a number value $number")
+                finalList.add(0,
+                    MainEntity("$number","Entity number $number", "price", "reais", "url")
+                )
+
+                emit(
+                    Resource.success(finalList)
+                )
+
             }
+        }
 
-            override suspend fun getFromDB(): List<MainEntity>? {
-                val mainEntityList = appDao.getMainEntityList()
-                return if(mainEntityList.isNotEmpty()){
-                    mainEntityList
-                }else{
-                    null
-                }
-            }
-
-            override fun saveIntoDB(resultData: List<MainEntity>?) {
-                appDao.saveMainEntityList(resultData)
-            }
-
-        }.fromHttpOnly()
     }
 
     suspend fun mainEntityAdditional(): Resource<List<MainEntity>> {
